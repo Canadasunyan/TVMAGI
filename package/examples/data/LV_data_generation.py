de2ebb3b0cfec074d2a1dcd63d10076c11998e08
Canadasunyan <37202740+Canadasunyan@users.noreply.github.com>
2022-03-03 23:56:16 -0500

    Update LV_data_generation.py

--- package/examples/data/LV_data_generation.py
+++ package/examples/data/LV_data_generation.py
@@ -11,22 +11,6 @@ gamma = 1 + 0.1 * np.sin(6.28 * X / 10)
 theta_true = np.vstack([alpha, gamma]).T
 
 # data generation for 20 years with monthly observations
-def recover_data(alpha, beta, delta, gamma, days, obs_per_day, state0=[3., 1.], linspace=1000, noise=True):
-    nobs = days
-    step_size = 1. / (linspace * obs_per_day)
-    state_ls = np.ones((days * linspace * obs_per_day, 2))
-    state_ls[0] = state0.copy()
-    for i in range(1, linspace * days * obs_per_day):
-        index = i - 1
-        state_ls[i][0] = state_ls[i - 1][0] + step_size * (
-                    alpha[index] * state_ls[i - 1][0] - beta * state_ls[i - 1][0] * state_ls[i - 1][1])
-        state_ls[i][1] = state_ls[i - 1][1] + step_size * (
-                    delta * state_ls[i - 1][0] * state_ls[i - 1][1] - gamma[index]  * state_ls[i - 1][1])
-    states = state_ls[::linspace]
-
-    return states
-
-true_x = recover_data(alpha, beta, delta, gamma, days, obs_per_day)
 
 def gen_data(idx, alpha, beta, delta, gamma, days, obs_per_day, state0=[3., 1.], linspace=1000, noise=True):
     nobs = days
